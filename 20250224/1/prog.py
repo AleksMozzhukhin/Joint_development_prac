8cfc56e89a6234dab8e2409178800c975fa248e3
Aleks_mozz laptop WSL <s02220179@gse.cs.msu.ru>
2025-02-27 20:24:00 +0300

    <HW 27_02 Task1> final solution

--- 20250224/1/prog.py
+++ 20250224/1/prog.py
@@ -1,5 +1,5 @@
 import sys
-
+import cowsay
 
 class MUD:
     def __init__(self):
@@ -32,12 +32,42 @@ class MUD:
         else:
             print("Invalid command")
 
-    def move_player(self, cmd):
-        print(f'Done command: {cmd}')
+    def move_player(self, direction):
+        if direction == "up":
+            self.player_y = (self.player_y - 1) % self.grid_size
+        elif direction == "down":
+            self.player_y = (self.player_y + 1) % self.grid_size
+        elif direction == "left":
+            self.player_x = (self.player_x - 1) % self.grid_size
+        elif direction == "right":
+            self.player_x = (self.player_x + 1) % self.grid_size
+        else:
+            print("Invalid command")
+            return
+
+        print(f"Moved to ({self.player_x}, {self.player_y})")
+
+        # Check if there's a monster at the new position
+        if (self.player_x, self.player_y) in self.monsters:
+            self.encounter(self.player_x, self.player_y)
 
     def add_monster(self, x, y, hello):
-        print(f"Added monster to ({x}, {y}) saying {hello}")
+        if not (0 <= x < self.grid_size and 0 <= y < self.grid_size):
+            print("Invalid arguments")
+            return
 
+        if (x, y) in self.monsters:
+            print(f"Added monster to ({x}, {y}) saying {hello}")
+            print("Replaced the old monster")
+        else:
+            print(f"Added monster to ({x}, {y}) saying {hello}")
+
+        self.monsters[(x, y)] = hello
+
+    def encounter(self, x, y):
+        if (x, y) in self.monsters:
+            monster_hello = self.monsters[(x, y)]
+            print(cowsay.cowsay(monster_hello))
 
 game = MUD()
 

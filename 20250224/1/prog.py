63ab38ae722fe7be62390e3c2e725d87ed8a5e27
Aleks_mozz laptop WSL <s02220179@gse.cs.msu.ru>
2025-02-27 19:54:50 +0300

    <HW 27_02 Task1> parse comand

--- 20250224/1/prog.py
+++ 20250224/1/prog.py
@@ -9,8 +9,34 @@ class MUD:
         self.monsters = {}
 
     def process_command(self, command):
-        print(command)
-        return
+        if not command:
+            return
+
+        parts = command.strip().split()
+        cmd = parts[0].lower()
+
+        if cmd in ["up", "down", "left", "right"]:
+            self.move_player(cmd)
+        elif cmd == "addmon":
+            if len(parts) < 4:
+                print("Invalid arguments")
+                return
+
+            try:
+                x = int(parts[1])
+                y = int(parts[2])
+                hello = parts[3]
+                self.add_monster(x, y, hello)
+            except ValueError:
+                print("Invalid arguments")
+        else:
+            print("Invalid command")
+
+    def move_player(self, cmd):
+        print(f'Done command: {cmd}')
+
+    def add_monster(self, x, y, hello):
+        print(f"Added monster to ({x}, {y}) saying {hello}")
 
 
 game = MUD()

045f885bc51a3f0458a6dba49ff8c15763ea9fe9
Aleks_mozz laptop WSL <s02220179@gse.cs.msu.ru>
2025-04-28 13:27:43 +0300

    <CW 11_AutomationBuild> add erase script

--- 20250428/0/dodo.py
+++ 20250428/0/dodo.py
@@ -1,5 +1,14 @@
+from pathlib import Path
+
 def task_docs():
     """Builds documentation"""
     return{
+        'file_dep': [list(Path(".").glob("*.py"))],
         "actions": ["sphinx-build -M html docs/source _build"],
+    }
+
+def task_erase():
+    """Erase all generated and new files"""
+    return {
+        'actions': ['git reset --hard', 'git clean -xdf']
     }
